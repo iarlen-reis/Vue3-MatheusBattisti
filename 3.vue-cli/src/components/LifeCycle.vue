98a8a8dc7473846c4966eb188b1938a013d90b09
Iarlen <iarlensr@hotmail.com>
2023-04-16 14:58:35 -0300

    aprendendo lifeCycles hooks.

---
+++ 3.vue-cli/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Meu nome: {{ name }}</h1>
+</template>
+
+<script>
+export default {
+  name: "LifeCycle",
+  data() {
+    return {
+      name: "Ainda não sei.",
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.name = "Iarlen";
+    }, 1000);
+  },
+  mounted() {
+    setTimeout(() => {
+      this.name = "Feliz";
+    }, 2000);
+  },
+};
+</script>
