bed1eec31ef9de58150ce8349e5b5dee35f521ec
Iarlen <iarlensr@hotmail.com>
2023-04-16 14:38:21 -0300

    aprendendo a adicionar dados em componentes.

--- 3.vue-cli/src/App.vue
+++ 3.vue-cli/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <h1>Olá Mundo!</h1>
   <PrimeiroComponente />
 </template>
 
 <script>
-  import PrimeiroComponente from './components/PrimeiroComponente.vue';
+import PrimeiroComponente from "./components/PrimeiroComponente.vue";
 
-  export default {
-    name: 'App',
-    components: {
-      PrimeiroComponente,
-    }
-  }
+export default {
+  name: "App",
+  components: {
+    PrimeiroComponente,
+  },
+};
 </script>
--- 3.vue-cli/src/components/PrimeiroComponente.vue
+++ 3.vue-cli/src/components/PrimeiroComponente.vue
@@ -1,9 +1,18 @@
 <template>
+  <div>
     <h1>Olá Vue!</h1>
+    <p>Meu nome é {{ name }}, e minha idade é {{ age }} anos.</p>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'PrimeiroComponente'
-    }
+export default {
+  name: "PrimeiroComponente",
+  data() {
+    return {
+      name: "Iarlen",
+      age: 21,
+    };
+  },
+};
 </script>
