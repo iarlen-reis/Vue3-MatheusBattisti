95d4c9d62ef0c46f7792bb63688ac663d5a12b54
Iarlen <iarlensr@hotmail.com>
2023-04-16 15:44:48 -0300

    aprendendo componetização.

---
+++ 3.vue-cli/src/components/Pessoa.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Essa é a descrição da pessoa: {{ name }}</h2>
+    <Descricao />
+  </div>
+</template>
+<script>
+import Descricao from "./Descricao.vue";
+
+export default {
+  name: "Pessoa",
+  components: {
+    Descricao,
+  },
+};
+</script>
